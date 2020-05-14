93f4f69b6207ac4cf559e2298450acfcd8a5523f
harssergio <hsergiou11@gmail.com>
2020-05-14 18:41:45 +0300

    Replaced the base R histogram with a ggplot2 histogram

--- script.R
+++ script.R
@@ -8,3 +8,15 @@ students_df <-read.csv("C:/gittut/data/students.csv")
 summary(students_df)
 # View a histogram of the absences variable
 hist(students_df$absences)
+
+# Load libraries
+library(ggplot2)
+# Load students dataset
+students_df <- read.csv("data/students.csv")
+# Look at a summary() of students3
+summary(students_df)
+# View a histogram of the absences variable
+ggplot(students_df, aes(students_df$absences)) +
+  geom_histogram() +
+  labs(title = "Histogram for absences of Students") +
+  labs(x = "Absences", y = "Count")
